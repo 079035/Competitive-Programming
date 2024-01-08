b34009967e913f0ce1c6f614c92fba3984497c75
079035 <62355576+079035@users.noreply.github.com>
2024-01-07 23:44:09 -0600

    Create 11727.cc

---
+++ Baekjoon/11727/11727.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    int n;
+    std::cin>> n;
+    std::vector<int> dp(n + 1, 0);
+    dp[1] = 1;
+    dp[2] = 3;
+    for (int i = 3; i <= n; ++i) {
+        dp[i] = (dp[i - 1] + dp[i - 2] * 2)%10007;
+    }
+    std::cout<< dp[n] % 10007<< '\n';
+}
